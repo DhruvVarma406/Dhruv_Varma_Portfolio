f6b96dd4d658cea9359935879a39078a1c192482
Dhruv Varma <varmadhruv406@gmail.com>
2026-08-11 16:09:00 +0530

    Configure Render allowed host

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -27,8 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True") == "True"
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
-
+ALLOWED_HOSTS = [
+    "dhruv-varma-portfolio.onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
 # Application definition
 
 INSTALLED_APPS = [
